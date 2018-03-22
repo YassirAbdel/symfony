7b178c5390d45888d85677fd0ae3b7eb19de7258
abdel <yassir.montet70@gmail.com>
2018-03-22 21:00:08 +0100

    vesrion 22-03-2018

--- src/OC/PlatformBundle/Form/AdvertEditType.php
+++ src/OC/PlatformBundle/Form/AdvertEditType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 
-class AdvertEditType2 extends AbstractType
+class AdvertEditType extends AbstractType
 {
     public function getParent ()
     {
